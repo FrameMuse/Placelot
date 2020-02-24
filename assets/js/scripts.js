58f103aa9c578386cf03aa3475cf3973fa141a5e
FrameMuse <minicablestone@gmail.com>
2020-02-24 20:18:05 +0400

    v0.62

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,19 @@
 $(".reveal-block__summary").click(function () {
     $(this).parent().toggleClass("reveal-block--revealed");
 });
+
+
+var topbar_wallet_cancel = false;
+$(".topbar-wallet").on({
+    mouseenter: function () {
+        topbar_wallet_cancel = true;
+        $(".topbar-wallet__dropdown-menu").css({ display: "grid" });
+    },
+    mouseleave: function () {
+        topbar_wallet_cancel = false;
+        setTimeout(() => {
+            if (topbar_wallet_cancel) return;
+            $(".topbar-wallet__dropdown-menu").css({ display: "" });
+        }, 250);
+    }
+});
