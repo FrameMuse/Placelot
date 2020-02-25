d14aeeb12705481661cf54831e339ab9207a86a0
FrameMuse <minicablestone@gmail.com>
2020-02-25 20:29:27 +0400

    v0.63

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,13 +9,21 @@ var topbar_wallet_cancel = false;
 $(".topbar-wallet").on({
     mouseenter: function () {
         topbar_wallet_cancel = true;
-        $(".topbar-wallet__dropdown-menu").css({ display: "grid" });
+        $(".topbar-wallet__dropdown-menu").css({ display: "grid" }).animate({
+            opacity: 1,
+            top: "4em",
+        }, 300);
     },
     mouseleave: function () {
         topbar_wallet_cancel = false;
         setTimeout(() => {
             if (topbar_wallet_cancel) return;
-            $(".topbar-wallet__dropdown-menu").css({ display: "" });
-        }, 250);
+            $(".topbar-wallet__dropdown-menu").animate({
+                opacity: 0,
+                top: "7em",
+            }, 300, function () {
+                $(this).css({ display: "" });
+            });
+        }, 350);
     }
 });
