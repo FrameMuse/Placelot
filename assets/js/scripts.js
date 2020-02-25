81c4c922af9d562b9ef905971a5dab0c3f501967
FrameMuse <minicablestone@gmail.com>
2020-02-25 22:05:55 +0400

    v0.64

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,71 @@
+// Classes
+
+class popup {
+    static on = { };
+    static tend(option) {
+        var element = ".popup-window__" + option;
+        return $(element);
+    }
+
+    static wEdit(options = {}) {
+        for (var option in options) {
+            this.tend(option).html(options[option]);
+        }
+    }
+
+    static fadeIn(obj) {
+        $(".popup").removeAttr("hidden");
+        setTimeout(() => {
+            obj.removeClass("scale-out scale-in").addClass("scale-in")
+        }, 50);
+    }
+
+    static fadeOut() {
+        var popup = $(".popup");
+        popup.find("> *:not(.popup__cover)").removeClass("scale-out scale-in").addClass("scale-out");
+        setTimeout(() => {
+            return popup.attr("hidden", "");
+        }, 200);
+    }
+
+    static open($window, options = {}) {
+        // Clearing
+        this.wEdit({
+            title: null,
+            summary: null,
+            content: null,
+        });
+
+        this.on[$window].apply(this, options);
+
+        // Animation
+        this.fadeIn($(".popup-window"));
+    }
+
+    static close(force = false) {
+        if (force) this.fadeOut();
+        $(".popup-window__close, .popup__cover").click();
+    }
+}
+
 // Profile
 
 $(".reveal-block__summary").click(function () {
     $(this).parent().toggleClass("reveal-block--revealed");
 });
 
-
-var topbar_wallet_cancel = false;
-$(".topbar-wallet").on({
-    mouseenter: function () {
-        topbar_wallet_cancel = true;
-        $(".topbar-wallet__dropdown-menu").css({ display: "grid" }).animate({
-            opacity: 1,
-            top: "4em",
-        }, 300);
-    },
-    mouseleave: function () {
-        topbar_wallet_cancel = false;
-        setTimeout(() => {
-            if (topbar_wallet_cancel) return;
-            $(".topbar-wallet__dropdown-menu").animate({
-                opacity: 0,
-                top: "7em",
-            }, 300, function () {
-                $(this).css({ display: "" });
-            });
-        }, 350);
-    }
+$(".topbar-wallet").click(function () {
+    $(".topbar-wallet__dropdown-menu").toggleClass("topbar-wallet__dropdown-menu--active");
 });
+
+$(document).on("click", ".popup-window__close, .popup__cover", () => {
+    popup.fadeOut();
+});
+
+popup.on["profile_settings"] = function () {
+    this.wEdit({
+        title: "Header",
+        summary: "Summary",
+        content: "Content",
+    });
+};
